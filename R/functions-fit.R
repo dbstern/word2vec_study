1d455433cd0cc2675c7363963d7c7708927cb305
dbstern <debystern@gmail.com>
2018-09-30 15:06:33 -0300

    - bow_xgweight: normalizations of number of words per text and weights obtained from xgboost + bow using indicator instead of counter of words per text - fit_lasso/fit_xgb : unnest lists created - remove model_list

--- R/functions-fit.R
+++ R/functions-fit.R
@@ -4,110 +4,110 @@ gen_train <- function(size, seed = 1234, prob = .5) {
   return(train)
 }
 
-bow_xgweight <- function(model, bow, w2v) {
+bow_xgweight <- function(model, bow, w2v, indic_bow = T, norm_bow = T, norm_weigh = T) {
   xgimportance <- xgb.importance(
     colnames(bow), model  = model)
   
   words <- xgimportance$Feature
   gain <- xgimportance$Gain %>%
     diag(., nrow = length(.)) %>% as(.,"sparseMatrix")
+  bow <- bow[,words]
   
-  w2v_bow_wxgb <- bow[,words] %*% gain
-  if(!is.null(w2v))
-    w2v_bow_wxgb <- w2v_bow_wxgb %*% w2v[words,]
-  return(w2v_bow_wxgb)
+  if(indic_bow) bow <- (bow > 0)
+  if(norm_bow) bow <- bow / rowSums(bow)
+  
+  wxgb <- bow %*% gain
+  if(norm_weigh) wxgb <- wxgb * (rowSums((bow > 0) %*% gain) ^ (-1))
+  if(!is.null(w2v)) wxgb <- wxgb %*% w2v[words,]
+  return(wxgb)
 }
 
-model_lasso <- function(x, y, train, file_out, seed = NULL) {
-  model <- list()
+fit_lasso <- function(x, y, train, file_out, description=NA, seed = NULL, with_grid = F) {
+  flog.info("START: Ajuste lasso%s - %s - seed %s",
+            ifelse(with_grid, " com grid", ""), description, seed)
+  
+  fit <- list()
   if(file.exists(file_out)) {
     flog.info("Carregando %s", file_out)
-    model <- readRDS(file_out)
+    fit <- readRDS(file_out)
   }
   
   xtrain <- x[train,]; ytrain <- y[train]
   xtest <- x[!train,]; ytest <- y[!train]
   
-  flog.info(paste("Estimando Lasso (seed -",seed,")"))
-  set.seed(seed = seed)
-  ftry({
-    model$lasso <- cv.glmnet(
-      x = xtrain, y = ytrain, family = "binomial",
-      type.measure = "class", standardize = FALSE)
-  })
-  saveRDS(model, file = file_out)
-  if(is.null(model$lasso)) return(model)
-  
-  ftry({
-    model$lasso$test <- as.numeric(predict(
-      model$lasso, xtest, type = "response", s = "lambda.min"))
-  })
-  saveRDS(model, file = file_out)
-  if(is.null(model$lasso$test)) return(model)
-  
-  flog.info("ROC Lasso")
-  ftry({
-    model$lasso$roc <- roc(ytest, model$lasso$test)
-  })
-  saveRDS(model, file = file_out)
-  return(model)
+  if(!"lasso" %in% names(fit)) {
+    flog.info(paste0("Estimando Lasso (seed - ",seed,")"))
+    lambda <- if(with_grid) c(0.5^(2:20),0) else NULL
+    set.seed(seed = seed)
+    ftry({
+      fit$lasso <- cv.glmnet(
+        x = xtrain, y = ytrain, family = "binomial", type.measure = "class",
+        standardize = FALSE, lambda = lambda)
+      saveRDS(fit, file = file_out)
+    })
+  }
+  if(!"test" %in% names(fit)) {
+    if(is.null(fit$lasso)) return(fit)
+    ftry({
+      fit$test <- as.numeric(predict(
+        fit$lasso, xtest, type = "response", s = "lambda.min"))
+      saveRDS(fit, file = file_out)
+    })
+  }
+  if(!"roc" %in% names(fit)) {
+    flog.info("ROC Lasso")
+    ftry({
+      fit$roc <- roc(ytest, fit$test)
+      saveRDS(fit, file = file_out)
+    })
+  }
+  flog.info("END")
+  return(fit)
 }
 
-model_xgboost <- function(x, y, train, file_out, seed = NULL, no_cores = 1) {
-  model <- list()
+fit_xgboost <- function(x, y, train, file_out, description=NA, seed = NULL, no_cores = 1) {
+  flog.info("START: Ajuste xgboost - %s - seed %s", description, seed)
+  
+  fit <- list()
   if(file.exists(file_out)) {
     flog.info("Carregando %s", file_out)
-    model <- readRDS(file_out)
+    fit <- readRDS(file_out)
   }
   
   xtrain <- x[train,]; ytrain <- y[train]
   xtest <- x[!train,]; ytest <- y[!train]
   
-  flog.info(paste("Estimando bst(CV) (seed -",seed,")"))
-  set.seed(seed = seed)
-  ftry({
-    model$bstCV <- xgb.cv(
-      data = xtrain, label = ytrain, nthread = no_cores, nround = 10000,
-      nfold = 2, metrics = "error", objective = "binary:logistic",
-      lambda = 0.01, verbose = FALSE)
-  })
-  saveRDS(model, file = file_out)
-  
-  flog.info(paste("Estimando bst (seed -",seed,")"))
-  nround <- which.min(model$bstCV$evaluation_log$test_error_mean)
-  set.seed(seed = seed)
-  ftry({
-    model$bst <- xgboost(
-      data = xtrain, label = ytrain, nthread = no_cores, nround = nround,
-      objective = "binary:logistic", lambda = 0.01, verbose = FALSE)
-  })
-  saveRDS(model, file = file_out)
-  ftry({
-    model$bst$test <- as.numeric(predict(model$bst, xtest, type = "response"))
-  })
-  saveRDS(model, file = file_out)
-  
-  flog.info("ROC bst")
-  ftry({
-    model$bst$roc <- roc(ytest, model$bst$test)
-  })
-  saveRDS(model, file = file_out)
-  return(model)
-}
-
-model_list <- function(x, y, train, file_out, seed = NULL, no_cores = 1, description = NULL) {
-  model <- list(description = description)
-  if(file.exists(file_out))
-    model <- readRDS(file_out)
-  saveRDS(model, file = file_out)
-  
-  # evitar repetir calculos
-  # talvez adicionar um parametro para saber se deseja sobrescrever fits anteriores
-  if(!"lasso" %in% names(model))
-    model <- model_lasso(
-      x = x, y = y, train = train, file_out = file_out, seed = seed)
-  if(!"bst" %in% names(model))
-    model <- model_xgboost(
-      x = x, y = y, train = train, file_out = file_out, no_cores = no_cores)
-  return(model)
+  if(!"xgbcv" %in% names(fit)) {
+    flog.info(paste0("xgb.cv (seed - ",seed,")"))
+    set.seed(seed = seed)
+    ftry({
+      fit$xgbcv <- xgb.cv(
+        data = xtrain, label = ytrain, nthread = no_cores, nround = 10000,
+        nfold = 2, metrics = "error", objective = "binary:logistic",
+        lambda = 0.01, verbose = FALSE)
+      saveRDS(fit, file = file_out)
+    })
+  }
+  if(!"xgb" %in% names(fit)) {
+    flog.info(paste0("xgboost (seed - ",seed,")"))
+    nround <- which.min(fit$xgbcv$evaluation_log$test_error_mean)
+    set.seed(seed = seed)
+    ftry({
+      fit$xgb <- xgboost(
+        data = xtrain, label = ytrain, nthread = no_cores, nround = nround,
+        objective = "binary:logistic", lambda = 0.01, verbose = FALSE)
+      saveRDS(fit, file = file_out)
+    })
+  }
+  if(!"test" %in% names(fit)) {
+    fit$test <- as.numeric(predict(fit$xgb, xtest, type = "response"))
+    saveRDS(fit, file = file_out)
+  }
+  if(!"roc" %in% names(fit)) {
+    flog.info("ROC xgboost")
+    fit$roc <- roc(ytest, fit$test)
+    saveRDS(fit, file = file_out)
+  }
+  flog.info("END")
+  return(fit)
 }
