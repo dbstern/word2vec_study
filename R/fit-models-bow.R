f473ada980f9c1c15117b7692bd98acb330dd5cd
dbstern <debystern@gmail.com>
2018-10-17 00:33:44 -0300

    + plots to compare auc/roc between different clusters

--- R/fit-models-bow.R
+++ R/fit-models-bow.R
@@ -22,7 +22,6 @@ rm(cases_sample,cases_bow,cases_rm)
 # create directories where models will be saved
 path_out <- file.path(".","output","model")
 if(!dir.exists(path_out)) dir.create(path_out, recursive = T)
-ggpath <- file.path(".","plots","model-performance")
 
 ## fit models: lasso + xgboost ----
 t <- cases %>% split(., .$bow_file) %>%
@@ -58,17 +57,28 @@ t <- cases %>% split(., .$bow_file) %>%
       })
   })
 
-## compare auc between clusters ----
+## global ggplot - compare clusters ----
+ggpath <- file.path(".","plots","model-performance")
+
+bow <- readRDS(file.path("output","bow","bow.rds")) %>% as(.,"sparseMatrix")
+nbow <- ncol(bow)
+rm(bow)
+
+cases_sample <- file.path(".","output","track_sample.txt") %>%
+  read.table(., header = T, stringsAsFactors = F) %>%
+  dplyr::rename_all(., .funs = function(x) paste0("sample_", x))
 cases_bow <- file.path(".","output","track_bow.txt") %>%
   read.table(., header = T, stringsAsFactors = F) %>%
   dplyr::select(., everything(), file = file_out) %>%
   dplyr::rename_all(., .funs = function(x) paste0("bow_", x))
 cases_model <- file.path(".","output","track_model-bow.txt") %>%
   read.table(., header = T, stringsAsFactors = F)
-cases <- merge(cases_bow, cases_model) %>%
-  mutate(bow_k = ifelse(is.na(bow_k), 0, bow_k))
-rm(cases_model, cases_bow)
+cases <- merge(cases_sample,cases_bow) %>%
+  merge(.,cases_model) %>%
+  mutate(bow_k = ifelse(is.na(bow_k), nbow, bow_k))
+rm(cases_sample,cases_model, cases_bow)
 
+## compare auc ----
 cases$auc <- cases %>% 
   split(., seq(nrow(.))) %>%
   lapply(., function(x) {
@@ -77,7 +87,41 @@ cases$auc <- cases %>%
   }) %>%
   unlist()
 
-gg <- cases %>% ggplot(., aes(bow_k, auc))
-gg + geom_point() + facet_grid(model ~ .)
-gg + geom_point(aes(col = model), position = position_jitter(w = 10, h = 0))
-ggsave2(filename = "auc_model-bow_npl-cluster", path = ggpath)
+gg <- cases %>% split(., .$sample_igeq) %>%
+  lapply(., function(cases) {
+    title <- with(
+      cases[1,], paste("Sample: igeq",sample_igeq,"\nCluster method:",bow_method))
+    
+    gg <- cases %>% ggplot(., aes(bow_k, auc))
+    # gg + geom_point() + facet_grid(model ~ sample_prob)
+    gg + geom_point(aes(col = model), position = position_jitter(w = 10, h = 0)) +
+      facet_grid(sample_prob ~ .) + labs(title = title, x = "# clusters", y = "AUC")
+    paste0("auc_model-bow_npl-cluster_sample-igeq-",cases$sample_igeq[1]) %>%
+      ggsave2(filename = ., path = ggpath)
+  })
+
+## compare roc ----
+gg <- cases %>%
+  dplyr::filter(., bow_k%%400==0 | bow_k==nbow) %>%
+  split(., c(.$sample_id, .$bow_cluster, .$bow_method, .$seed)) %>%
+  lapply(., function(cases) {
+    roc <- cases %>%
+      split(., seq(nrow(.))) %>%
+      lapply(., function(x) {
+        if(!file.exists(x$file_out)) return(NA)
+        readRDS(x$file_out)[["roc"]] %>%
+          "["(c("sensitivities","specificities")) %>%
+          do.call(cbind, .) %>% as.data.frame() %>% cbind(x,.)
+      }) %>%
+      do.call(rbind, .)
+    
+    title <- with(
+      cases[1,], paste("Sample: train-prob",sample_prob,"| igeq",sample_igeq,"\nCluster method:",bow_method))
+    ggplot(data = roc, aes(specificities,sensitivities,
+                           group=file_out,col=bow_k,linetype=model)) +
+      geom_line() + labs(title = title, x = "Especificidade", y = "Sensibilidade")
+    paste0("roc_model-bow_npl-cluster_sample-id-",cases$sample_id[1]) %>%
+      ggsave2(filename = ., path = ggpath)
+  })
+
+
