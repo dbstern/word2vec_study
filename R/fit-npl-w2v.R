ba66cd4079e0fedcf585a0acfd3addc00269d7f9
dbstern <debystern@gmail.com>
2018-11-05 14:20:02 -0200

    bugfix: unlist words in review + skip cases already run

--- R/fit-npl-w2v.R
+++ R/fit-npl-w2v.R
@@ -1,21 +1,25 @@
 ## dependencias ----
 source(file.path("R","dependencies.R"))
 
-## global ----
+## fit word2vec to reviews ----
 path_out <- file.path(".","output","w2v")
 if(!dir.exists(path_out)) dir.create(path_out, recursive = T)
 
 cases <- expand.grid(
   ncores = no_cores,
-  dim = c(100,300),
+  dim = c(2,50,100,300),
   seed = 1:1,
   window = 12,
   iter = 10,
   negsamples = 0,
   mincount = 1000
 )
+cases_rm <- file.path(".","output","track_w2v.txt") %>%
+  read.table(., header = T, stringsAsFactors = F)
+cases <- merge(cases, cases_rm, all = T) %>%
+  dplyr::filter(., is.na(file_out))
+rm(cases_rm)
 
-## gerar os modelos word2vec ----
 x <- split(cases, seq(nrow(cases))) %>%
   lapply(., function(case) {
     
@@ -38,34 +42,49 @@ x <- split(cases, seq(nrow(cases))) %>%
     ))
     return(NULL)
 })
-
-## gerar matriz de representação do texto ----
 rm(cases)
+
+## generate represation matrix per review ----
+path_out <- file.path(".","output","text-w2v")
+if(!dir.exists(path_out)) dir.create(path_out, recursive = T)
+
 cases_w2v <- file.path(".","output","track_w2v.txt") %>%
   read.table(., header = T, stringsAsFactors = F) %>%
   dplyr::select(., everything(), file = file_out) %>%
   dplyr::rename_all(., .funs = function(x) paste0("w2v_", x))
+cases_rm <- file.path(".","output","track_text-w2v.txt") %>%
+  read.table(., header = T, stringsAsFactors = F)
 
 data <- file.path("input","reviews_clean.txt") %>%
   read.table(., header = F, sep = "\t", stringsAsFactors = F)
 
 r <- cases_w2v %>% split(., seq(nrow(.))) %>%
   lapply(., function(case) {
-  w2v <- case$w2v_file %>% read.vectors(.)
-  
-  r <- seq(nrow(data)) %>%
-    purrr::map(., function(idx) {
-      file <- paste0("w2v",case$w2v_dim,"_text",idx) %>%
-        namefile(path = path_out, str = ., extension = ".bin")
-      case <- data.frame(text = idx, w2v_file = case$w2v_file, file_out = file)
-      file.path(".","output","track_text-w2v.txt") %>%
-        write.table(x = case, file = ., append = file.exists(.),
-                    col.names = !file.exists(.), row.names = F)
-      
-      words <- data[idx,] %>% str_split(., "[[:space:]]")
-      w2v[match(words, row.names(w2v), nomatch = 0),] %>%
-        write.binary.word2vec(., filename = file)
-    })
-  
-  NULL
-})
+    w2v <- case$w2v_file %>% read.vectors(.)
+    
+    text <- cases_rm %>%
+      dplyr::filter(w2v_file == case$w2v_file) %>%
+      dplyr::select(text) %>% unlist(.) %>%
+      setdiff(seq(nrow(data)), .) 
+
+    r <- text %>%
+      purrr::map(., function(idx) {
+        file <- paste0("w2v",case$w2v_dim,"_text",idx) %>%
+          namefile(path = path_out, str = ., extension = ".bin")
+        case <- data.frame(text = idx, w2v_file = case$w2v_file, file_out = file)
+        file.path(".","output","track_text-w2v.txt") %>%
+          write.table(x = case, file = ., append = file.exists(.),
+                      col.names = !file.exists(.), row.names = F)
+        
+        words <- data[idx,] %>% str_split(., "[[:space:]]")
+        w2v[match(words[[1]], row.names(w2v), nomatch = 0),] %>%
+          write.binary.word2vec(., filename = file)
+      })
+    NULL
+  })
+rm(cases_rm,cases_w2v)
+
+# tt <- file.path(".","output","track_text-w2v.txt") %>%
+#   read.table(., header = T, stringsAsFactors = F) %>%
+#   head(1) %>%
+#   read.vectors(filename = as.character(.$file_out))
